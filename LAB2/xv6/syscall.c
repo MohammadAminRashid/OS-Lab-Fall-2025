97d9572bf1298aa699578f21b6f7bcd168a2987b
sadeghmahdavi <mhmdsmahdavi@gmail.com>
2025-11-01 16:16:56 +0330

    simple system call added

--- LAB2/xv6/syscall.c
+++ LAB2/xv6/syscall.c
@@ -103,6 +103,7 @@ extern int sys_unlink(void);
 extern int sys_wait(void);
 extern int sys_write(void);
 extern int sys_uptime(void);
+extern int sys_simple_arithmetic_syscall(void);
 
 static int (*syscalls[])(void) = {
 [SYS_fork]    sys_fork,
@@ -126,6 +127,7 @@ static int (*syscalls[])(void) = {
 [SYS_link]    sys_link,
 [SYS_mkdir]   sys_mkdir,
 [SYS_close]   sys_close,
+[SYS_simple_arithmetic_syscall] sys_simple_arithmetic_syscall,
 };
 
 void
